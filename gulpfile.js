a99e97790c703d4295f6114e74081ec51938df69
Monica Dinculescu <noms@google.com>
2017-01-10 12:02:43 -0800

    add a build file!

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp = require('gulp');
+var jeditor = require('gulp-json-editor');
+var merge = require('merge-stream');
+var zip = require('gulp-zip');
+
+var xpiName = 'picasso-google-calendar.xpi';
+var zipName = 'picasso-google-calendar.zip';
+
+gulp.task('default', function () {
+  var files = ['*', '!manifest.json'];
+  var manifest = gulp.src('manifest.json');
+  var dest = gulp.dest('bin/');
+
+  merge(gulp.src(files), manifest)
+    .pipe(zip(zipName))
+    .pipe(dest);
+
+  manifest = manifest.pipe(jeditor({
+    'applications': {
+      'gecko': {
+        'id': 'picasso-google-calendar@example'
+      }
+    }
+  }));
+
+  merge(gulp.src(files), manifest)
+    .pipe(zip(xpiName))
+    .pipe(dest);
+});
